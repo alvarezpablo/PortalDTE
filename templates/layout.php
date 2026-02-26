aa3722e6aaca2448851bc1303d659640beba20ac
alvarezpablo <alvarez.pablo.pa@gmail.com>
2026-02-26 12:28:37 -0300

    Test: cambiar titulo para verificar deploy

--- templates/layout.php
+++ templates/layout.php
@@ -159,7 +159,7 @@ $defaultPage = 'main.php';
 <head>
     <meta charset="ISO-8859-1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portal OpenDTE</title>
+    <title>Portal OpenDTE - NUEVO 2024</title>
     <link rel="shortcut icon" href="/favicon.ico">
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
