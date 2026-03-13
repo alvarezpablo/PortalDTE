a24df51e41dab5b19066778c3736f9130879e06d
alvarezpablo <alvarez.pablo.pa@gmail.com>
2026-03-13 13:18:18 -0300

    Corrige paginacion en contribuyentes v2

--- mantencion/form_cont_elec_v2.php
+++ mantencion/form_cont_elec_v2.php
@@ -65,8 +65,10 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+	    <link rel="shortcut icon" href="/favicon.ico">
     <meta charset="ISO-8859-1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <base href="<?php echo h($_LINK_BASE); ?>" />
     <title>Contribuyentes Electr&oacute;nicos - Portal DTE</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
